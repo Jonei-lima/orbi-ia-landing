d21aed09aedd9f30d31c9a2124bb0fd15c3ccf8a
Jonei Lima <joneilima@Joneis-MacBook-Air.local>
2026-02-13 12:35:30 -0300

    um teste novamente

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -5,7 +5,7 @@ const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
-
+  console.log("EVOLUTION_URL:", process.env.EVOLUTION_URL);
 export async function POST(req: Request) {
   try {
     const body = await req.json();
