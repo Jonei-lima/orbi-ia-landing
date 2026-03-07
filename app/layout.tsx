cb9562a251ea2f41717c6c9f86a241cce320bcac
Jonei-lima <jonei.lima@gmail.com>
2026-03-06 23:21:50 -0300

    Update Google Tag Manager script integration
    Replaced 'next/script' with standard <script> tags for Google Tag Manager integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,19 +15,20 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17997912287"
-          strategy="afterInteractive"
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17997912287"></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17997912287');
+            `,
+          }}
         />
 
-        <Script id="google-ads" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17997912287');
-          `}
-        </Script>
       </head>
 
       <body className="antialiased bg-white text-neutral-900">
